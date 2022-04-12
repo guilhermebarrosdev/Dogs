091207c24a3493170fa80f92e39d43aeabd98120
Guilherme Barros <wingui3@gmail.com>
2022-04-12 14:17:21 -0300

    :sparkles: Create file api

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,27 +1,39 @@
 import { Link } from 'react-router-dom'
-import useForm from '../../Hooks/useForm'
 import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import { TOKEN_POST, USER_GET } from '../../api'
 import Input from '../Forms/Input'
+import { useEffect } from 'react'
 
 const LoginForm = () => {
   const username = useForm()
   const password = useForm()
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      getUser(token)
+    }
+  }, [])
+
+  const getUser = async (token) => {
+    const { url, options } = USER_GET(token)
+    const response = await fetch(url, options)
+    const json = await response.json()
+    console.log(json)
+  }
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
     if (username.validate() && password.validate()) {
-      fetch(`https://www.dogsapi.origamimd.dev/json/jwt-auth/v1/token`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'Application/json',
-        },
-        body: JSON.stringify(),
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
       })
-        .then((response) => {
-          console.log(response)
-          return response.json()
-        })
-        .then((json) => console.log(json))
+      const response = await fetch(url, options)
+      const json = await response.json()
+      window.localStorage.setItem('token', json.token)
+      getUser(json.token)
     }
   }
   return (
