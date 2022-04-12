6614bb4e58f2b1177e265490d7cda95523f03176
Guilherme Barros <wingui3@gmail.com>
2022-04-12 12:49:49 -0300

    :sparkles: Add component Form with styles and create folder Hooks and file useForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,32 +1,37 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import useForm from '../../Hooks/useForm'
+import Button from '../Forms/Button'
+import Input from '../Forms/Input'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const username = useForm()
+  const password = useForm()
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch(`https://www.dogsapi.origamimd.dev/json/jwt-auth/v1/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/json'
-      },
-      body: JSON.stringify({username, password})
-    }).then(response => {
-      console.log(response)
-    return response.json()
-  }).then((json)=> console.log(json))
+    event.preventDefault()
+    if (username.validate() && password.validate()) {
+      fetch(`https://www.dogsapi.origamimd.dev/json/jwt-auth/v1/token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'Application/json',
+        },
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response)
+          return response.json()
+        })
+        .then((json) => console.log(json))
+    }
   }
-
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={({target})=> setUsername(target.value)} />
-        <input type="text" value={password} onChange={({target})=> setPassword(target.value)} />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
       </form>
-      <button>Entrar</button>
       <Link to="/login/criar">Cadastro</Link>
     </section>
   )
