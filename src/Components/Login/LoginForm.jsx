3881b1436813b08d13901fcf2cd28dbfc9343727
Guilherme Barros <wingui3@gmail.com>
2022-04-19 16:30:29 -0300

    :sparkles: Create folder Helper and file error, refactor LoginForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,6 +4,9 @@ import useForm from '../../Hooks/useForm'
 import Input from '../Forms/Input'
 import { useContext } from 'react'
 import { UserContext } from '../../UserContext'
+import Error from '../Helper/Error'
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 const LoginForm = () => {
   const username = useForm()
@@ -18,9 +21,9 @@ const LoginForm = () => {
     }
   }
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -28,9 +31,18 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastro</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </section>
   )
 }
